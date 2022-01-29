b330de0453ee7cd918626246fff46f43b4ae499d
tiagohmartins <tiago.martins2013@gmail.com>
2022-01-28 21:32:45 -0300

    cadastro e correção foot

---
+++ main.js
@@ -0,0 +1,8 @@
+function mostrarSenha(){
+    var tipo = document.getElementById("password");
+    if(tipo.type =="password"){
+        tipo.type="text";
+    }else{
+        tipo.type="password";
+    }
+}
